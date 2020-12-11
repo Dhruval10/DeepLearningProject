fb7c90861537752eb5a89b959be81c2287d68cb6
Dhruval <22280539+Dhruval10@users.noreply.github.com>
2020-12-10 16:26:46 -0800

    Create Housing_Price_Prediction_Model.py

---
+++ Housing_Price_Prediction_Model.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+%matplotlib inline
+sns.set(style='whitegrid')
+
+# Feature Selection
+from sklearn.feature_selection import RFE
+# Scaler
+from sklearn.preprocessing import MinMaxScaler
+
+# Modeling, Evaluation
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn import metrics
+import statsmodels.api as sm
+
+#Validation
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
